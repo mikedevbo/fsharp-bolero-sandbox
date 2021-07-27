29e8945b009c18f30002e31d14a0f3a944edf99a
mikedevbo <mike@devbo.pl>
2021-07-27 17:42:07 +0200

    1. add base solution structure 2. add simple service definition

---
+++ sandbox/9-remoting/src/Remoting.Contracts/Library.fs
@@ -0,0 +1,11 @@
+﻿module Remoting.Contracts
+
+open Bolero.Remoting
+
+type SimpleService =
+    {
+        GetHello: unit -> Async<string>
+    }
+
+    interface IRemoteService with
+        member this.BasePath = "/simpleservice"
